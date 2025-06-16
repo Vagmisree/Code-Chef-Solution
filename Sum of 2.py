5a14f3b1abdf48cab04a4398107a66daef8271f9
Vagmi sree katena <150500586+Vagmisree@users.noreply.github.com>
2025-06-16 15:43:01 +0530

    Create Sum of 2.py

---
+++ Sum of 2.py
@@ -0,0 +1,9 @@
+# Taking input from user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# Calculating sum
+total = a + b
+
+# Displaying result
+print("Sum of the two numbers is:", total)
